a3c9b868a8ef29a8967aa6f2a81312480feb8982
Stefan Thorburn <sathorb@gmail.com>
2021-03-06 17:42:34 -0500

    Added absyn class files, removed unnecessary files

---
+++ src/absyn/ArrayDec.java
@@ -0,0 +1,19 @@
+package src.absyn;
+
+public class ArrayDec extends VarDec {
+   NameTy type;
+   String name;
+   IntExp size;
+
+   public ArrayDec(int row, int col, NameTy type, String name, IntExp size) {
+      this.row = row;
+      this.col = col;
+      this.type = type;
+      this.name = name;
+      this.size = size;
+   }
+
+   public void accept( AbsynVisitor visitor, int level ) {
+      //visitor.visit( this, level );
+   }
+}
