062091fe9999219e8de202063b7c71e895777d60
Prabhleen Ratra <pratra@fred.socs.uoguelph.ca>
2021-03-16 22:23:49 -0400

    modified a file and added comments according to tips given

--- src/absyn/Exp.java
+++ src/absyn/Exp.java
@@ -2,3 +2,7 @@ package absyn;
 
 abstract public class Exp extends Absyn {
 }
+
+
+//Add a new attribute “Dec dtype” in “Exp.java” so that it can be shared by all the descendent classes: 
+//dtype is essentially a reference to a “Dec” node that helps us find the type information
