5f47c4310f806df4dfdb52c5ef59e1c1fcac701a
Stefan Thorburn <sathorb@gmail.com>
2021-03-22 18:08:27 -0400

    Added NodeType details

--- src/NodeType.java
+++ src/NodeType.java
@@ -1,8 +1,15 @@
 //Contains instance variables: “String name”,“Dec def”, “int level”, and a constructor
 
-public class NodeType {
-    
-    public NodeType(){
+import absyn.*;
 
+public class NodeType {
+    public String name;
+    public Dec def;
+    public int level;
+    
+    public NodeType(String name, Dec def, int level) {
+        this.name = name;
+        this.def = def;
+        this.level = level;
     }
 }
