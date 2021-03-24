c64cd68e7f552f2162f6b408e408f54fc9bf07cb
Stefan <sathorb@gmail.com>
2021-03-23 22:11:50 -0400

    distinguished int and int arrays in sym table

--- src/NodeType.java
+++ src/NodeType.java
@@ -6,12 +6,6 @@ public class NodeType {
     public String name;
     public Dec def;
     public int level;
-
-    public static final int NO_DEC = NameTy.VOID - 1;
-    public static final int VOID = NameTy.VOID;
-    public static final int INT_PRIM = NameTy.INT;
-    public static final int INT_ARR = NameTy.INT + 1;
-
     
     public NodeType(String name, Dec def, int level) {
         this.name = name;
