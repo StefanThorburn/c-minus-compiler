f982a2669c02f62cd963854387b1d2eb6798faf7
Stefan Thorburn <sathorb@gmail.com>
2021-04-04 21:14:56 -0400

    added stubs and tree traversal to CodeGenerator

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -1,10 +1,10 @@
 import absyn.*;
 
 public class CodeGenerator implements AbsynVisitor {
-    int IADDR_SIZE = 1024;
-    int DADDR_SIZE = 1024;
-    int NO_REGS = 8;
-    int PC_REG = 7;
+    private int IADDR_SIZE = 1024;
+    private int DADDR_SIZE = 1024;
+    private int NO_REGS = 8;
+    private int PC_REG = 7;
     public static int mainEntry = 0;
     public static int globalOffset = 0;
     // constructor for initialization and all emitting routines
@@ -19,6 +19,10 @@ public class CodeGenerator implements AbsynVisitor {
     /* functions to maintain code space: some methods like emitRO, emitRM, prelude, and finale need to be added */
     public static int emitLoc= 0;
     public static int highEmitLoc= 0;
+
+    public CodeGenerator() {
+        
+    }
     
     public static int emitSkip( int distance ) {
         int i = emitLoc;
@@ -71,5 +75,135 @@ public class CodeGenerator implements AbsynVisitor {
     }
     */
         //...
+
+    // Visit methods are currently all stubs except traversal is maintained.
+    // The offset values sent in 'accept' calls are not necessarily correct currently -- they are just copied from the ShowTreeVisitor for indentation.
+
+    public void visit (NameTy nameTy, int offset, boolean isAddr ) {
+
+    }
+
+    public void visit (IndexVar var, int offset, boolean isAddr ) {
+
+        var.index.accept( this, offset + 1, false);
+    }
+
+    public void visit (SimpleVar var, int offset, boolean isAddr ) {
+
+    }
+
+    public void visit (ArrayDec array, int offset, boolean isAddr ) {
+
+    }
+
+    public void visit (ErrorDec compoundList, int offset, boolean isAddr ) {
+
+    }    
+
+    public void visit (FunctionDec functionDec, int offset, boolean isAddr ) {
+
+        functionDec.params.accept(this, offset, false);
+        functionDec.body.accept(this, offset, false);
+    }
+
+    public void visit (SimpleDec dec, int offset, boolean isAddr ) {
+
+    }  
+
+    public void visit (DecList decList, int offset, boolean isAddr ) {
+
+        while( decList != null ) {
+            decList.head.accept( this, offset, false );
+            decList = decList.tail;
+        } 
+    }
+
+    public void visit (VarDecList decList, int offset, boolean isAddr ) {
+
+        while( decList != null ) {
+            decList.head.accept( this, offset, false );
+            decList = decList.tail;
+        } 
+    }
+
+    public void visit( ExpList expList, int offset, boolean isAddr ) {
+
+        if (expList.head != null) {
+            while( expList != null ) {
+                expList.head.accept( this, offset, false );
+                expList = expList.tail;
+            } 
+        }    
+    }  
+
+    public void visit( AssignExp exp, int offset, boolean isAddr ) {
+
+        if (exp.lhs != null) {
+            exp.lhs.accept( this, offset, false );
+        }
+        
+        exp.rhs.accept( this, offset, false );
+    }
+
+    public void visit (CallExp exp, int offset, boolean isAddr ) {
+
+        if (exp.args != null && exp.args.head != null) {
+            exp.args.accept(this, offset, false);
+        }    
+    }
+    
+    public void visit (CompoundExp compoundList, int offset, boolean isAddr ) {
+   
+        if (compoundList.decs != null) {
+            compoundList.decs.accept( this, offset+1, false );
+        }
+        if (compoundList.exps != null) {
+            compoundList.exps.accept( this, offset+1, false );
+        }    
+    }    
+
+    public void visit (ErrorExp compoundList, int offset, boolean isAddr ) {
+
+    }    
+
+    public void visit( IfExp exp, int offset, boolean isAddr ) {
+ 
+        exp.test.accept( this, offset, false );
+        exp.thenpart.accept( this, offset +1, false);
+
+        if (exp.elsepart != null ) {
+            exp.elsepart.accept( this, offset +1, false);
+        }               
+    }
+
+    public void visit( IntExp exp, int offset, boolean isAddr ) {
+
+    }
+
+    public void visit (NilExp exp, int offset, boolean isAddr ) {
+
+    }
+
+    public void visit( OpExp exp, int offset, boolean isAddr ) {
+
+        exp.left.accept( this, offset, false );
+        exp.right.accept( this, offset, false );
+    }
+
+    public void visit (ReturnExp exp, int offset, boolean isAddr ) {
+
+        exp.exp.accept(this, offset+1, false);
+    }  
+
+    public void visit( VarExp exp, int offset, boolean isAddr ) {
+
+        exp.variable.accept(this, offset+1, false);
+    }
+
+    public void visit (WhileExp exp, int offset, boolean isAddr ) {
+
+        exp.test.accept(this, offset+1, false);
+        exp.body.accept(this, offset+1, false);
+    }
 }
 
